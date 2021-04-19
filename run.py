c9f1d8059a03171b52f2efaa36d04a9768dc7cd0
Aap Afandi ID <69949735+KangPacman@users.noreply.github.com>
2021-04-20 04:04:49 +0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-if sys.version[0:3]=="3.9":
-	os.system("python 39.py")
-elif sys.version[0:3]=="3.8":
-	os.system("python 38.py")
-elif sys.version[0:3]=="2.7" or sys.version[0:3]=="2.8":
-	exit("[!] Gunakan Python Versi 3")
-else:
-	exit("[!] Versi Python Yang Lu Miliki Gak Support")
